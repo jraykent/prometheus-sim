1c47e35296d2d8d34fd7c3349ea85d0c68279c02
jraykent <kent_justin@icloud.com>
2025-06-26 22:17:26 -0400

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -1,21 +1,14 @@
 import random
 import time
 
-def run_simulation(personas, headlines, steps=10, api_func=None, auto_learn=True):
+def run_simulation(personas, headlines, steps=10, auto_learn=True):
     for i in range(steps):
         event = random.choice(headlines)
-        nlp_results = []
-        for persona in personas:
-            if api_func:
-                nlp = persona.nlp_headline_analysis(event[0], event[1], api_func=api_func)
-                nlp_results.append((event[0], event[1], nlp['sentiment'], nlp['surprise']))
-            else:
-                nlp_results.append(event)
         peer_ideologies = [p.ideology for p in personas]
         for idx, persona in enumerate(personas):
-            peer_mean = sum(peer_ideologies)/len(peer_ideologies)
+            peer_mean = sum(peer_ideologies) / len(peer_ideologies)
             peer_influence = (peer_mean - persona.ideology) * 0.2
-            persona.react(*nlp_results[idx], peer_influence=peer_influence)
+            persona.react(*event, peer_influence=peer_influence)
             if auto_learn:
                 persona.recover()
         time.sleep(0.01)
